12ec4dee407767e0c0f69c9e80c8800161fd7934
nantenucci <antenucci.nestor@gmail.com>
2026-08-15 10:05:13 -0300

    Fix: start_url y scope del manifest para subpath /tareas/

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'tareas-antenucci-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/tareas-icon-192.png', '/tareas-icon-512.png'];
+const ASSETS = ['/tareas/', '/tareas/index.html', '/tareas/manifest.json', '/tareas/tareas-icon-192.png', '/tareas/tareas-icon-512.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
